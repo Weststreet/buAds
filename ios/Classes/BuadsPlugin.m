e528bb10e99da36e753eaa3759f5a72aba272b5a
sxyberry@gmail.com <5800126866>
2019-08-05 19:14:25 +0800

    init

--- ios/Classes/BuadsPlugin.m
+++ ios/Classes/BuadsPlugin.m
@@ -5,19 +5,22 @@
 BuAds *buAds;
 UIViewController *mViewController;
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"buads"
-            binaryMessenger:[registrar messenger]];
-  BuadsPlugin* instance = [[BuadsPlugin alloc] init];
-  buAds=[[BuAds alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
-  mViewController = [[[[UIApplication sharedApplication]delegate] window]rootViewController];
-    
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"buads"
+                                     binaryMessenger:[registrar messenger]];
+    NSLog(@"registerWithRegistrar");
     RewardVerifyedStreamHandler* rewardVerifyedStreamHandler = [[RewardVerifyedStreamHandler alloc] init];
     FlutterEventChannel* eventChannel =
     [FlutterEventChannel eventChannelWithName:@"plugins.flutter.io/buads"
                               binaryMessenger:[registrar messenger]];
     [eventChannel setStreamHandler:rewardVerifyedStreamHandler];
+    
+    BuadsPlugin* instance = [[BuadsPlugin alloc] init];
+    buAds=[[BuAds alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+    mViewController = [[[[UIApplication sharedApplication]delegate] window]rootViewController];
+    
+    
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
